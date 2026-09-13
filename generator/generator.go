75d8716ea5b8d731ae1769278b5bf916191aace8
funfunpayer <sir.slayer83@gmail.com>
2026-09-13 23:25:35 +0200

    feat(generator): restore generator.go with --profile tf|tj and --roi2

--- generator/generator.go
+++ generator/generator.go
@@ -1,1 +1,5 @@
-PLACEHOLDER
+// Package generator - see main branch; this is a temporary note
+package generator
+
+// TEMP: full content being restored in next commit
+const TempNote = "restore in progress"
