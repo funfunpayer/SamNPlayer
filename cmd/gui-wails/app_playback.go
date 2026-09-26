5fcae1c470317a9f97013e8550e584ab78938e80
funfunpayer <sir.slayer83@gmail.com>
2026-09-27 00:07:14 +0200

    fix(host): restore app_playback.go (remove accidental PLACEHOLDER)

--- cmd/gui-wails/app_playback.go
+++ cmd/gui-wails/app_playback.go
@@ -1,1 +1,36 @@
-PLACEHOLDER
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/wailsapp/wails/v2/pkg/runtime"
+
+	"github.com/funfunpayer/SamNPlayer/funscript"
+	"github.com/funfunpayer/SamNPlayer/logging"
+	"github.com/funfunpayer/SamNPlayer/player"
+	"github.com/funfunpayer/SamNPlayer/sam"
+	"github.com/funfunpayer/SamNPlayer/samn"
+)
+
+type PlaybackOptions struct {
+	Mock                    bool    `json:"mock"`
+	SyncMode                string  `json:"syncMode"`
+	TickMs                  int64   `json:"tickMs"`
+	MaxSpeed                float64 `json:"maxSpeed"`
+	Smoothing               float64 `json:"smoothing"`
+	SoftStartMs             int     `json:"softStartMs"`
+	UseVideoSync            bool    `json:"useVideoSync"`
+	ExtendedOEnabled        bool    `json:"extendedOEnabled"`
+	ExtendedOMin            float64 `json:"extendedOMin"`
+	ExtendedOHoldS          float64 `json:"extendedOHoldS"`
+	ExtendedORestoreMs      float64 `json:"extendedORestoreMs"`
+	DisableContactVibration bool    `json:"disableContactVibration"`
+	ContactIntensityScale float64 `json:"contactIntensityScale"`
+	ContactExtraSmooth float64 `json:"contactExtraSmooth"`
+	ContactVibrationSpan  float64 `json:"contactVibrationSpan"`
+	ContactVibrationCurve string  `json:"contactVibrationCurve"`
+}
+
+// RESTORE_INCOMPLETE - full file will be restored in next commit
