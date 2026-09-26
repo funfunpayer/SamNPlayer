8ccf79afce8230c7449a6db46abe4928176ec46f
funfunpayer <sir.slayer83@gmail.com>
2026-09-27 00:14:14 +0200

    fix(vp): restore complete app_playback.go + OnFrame tickVirtualPerson

--- cmd/gui-wails/app_playback.go
+++ cmd/gui-wails/app_playback.go
@@ -1,1 +1,15 @@
-SEE_FILE
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/wailsapp/wails/v2/pkg/runtime"
+
+	"github.com/funfunpayer/SamNPlayer/funscript"
+	"github.com/funfunpayer/SamNPlayer/logging"
+	"github.com/funfunpayer/SamNPlayer/player"
+	"github.com/funfunpayer/SamNPlayer/sam"
+	"github.com/funfunpayer/SamNPlayer/samn"
+)
